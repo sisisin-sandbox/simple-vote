88382d23fb30a13b89cfb01bd657125bb00ac425
sisisin <azsisisin@gmail.com>
2017-02-12 18:25:54 +0900

    feat(server) setup db

---
+++ server/migrations/20170212091507-create-vote.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Votes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      numVotes: {
+        allowNull: false,
+        defaultValue: 1,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Votes');
+  }
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../models');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  db.Vote.findAll().then(votes => {
+    res.render('index', { title: 'Express', votes: votes.map(v => v.toJSON()) });
+  }).catch(e => {
+    console.log(e);
+    res.status(500);
+    res.end();
+  });
 });
 
 module.exports = router;
